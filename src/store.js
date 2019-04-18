a15c99d8200f23561c678b5696a3ebd409e1cfab
Geoff Gorham <geogor37@gmail.com>
2019-04-17 20:06:22 -0600

    Set up authentication in the back end

--- src/store.js
+++ src/store.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    user: null,
   },
   mutations: {
-
+    setUser(state, user) {
+      state.user = user;
+    },
   },
   actions: {
-
+    async register(context, data) {
+      try {
+        let response = await axios.post("/api/users", data);
+        context.commit('setUser', response.data);
+        return "";
+      } catch (error) {
+        return error.response.data.message;
+      }
+    },
+    async login(context, data) {
+      try {
+        let response = await axios.post("/api/users/login", data);
+        context.commit('setUser', response.data);
+        return "";
+      } catch (error) {
+        return error.response.data.message;
+      }
+    },
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col-sm">
+    <div v-if="user">
+      <div class="row">
+        <div class="col-sm">
+          <h1>My Wishlists</h1>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="centered">
+        <p><strong>In order to create or view lists, you must register for an account or login.</strong></p>
+        <div class="centered">
+          <router-link to="/register" class="pure-button">Register</router-link>
+          or
+          <router-link to="/login" class="pure-button">Login</router-link>
+        </div>
+      </div>
+    </div>
+    <!--    <div class="row">-->
+    <!--      <form @submit.prevent="createList">-->
+    <!--        <input type="text" v-model="newListName" placeholder="List Name">-->
+    <!--        <button type="submit">Add List</button>-->
+    <!--      </form>-->
+    <!--    </div>-->
+    <!--    <br>-->
+    <!--    <h4 v-show="lists.length === 0" style="font-style: italic">You have no wishlists! Add one to get started.</h4>-->
+    <!--    <div v-for="list in lists" class="wishlist" v-bind:class="{ selected: list === selectedList }" @click="toggleSelectedList(list)">-->
+    <!--      <p><b>{{ list.name }}</b> - {{ list.items.length }} items</p>-->
+    <!--      <button @click.stop="deleteList(list)">Delete</button>-->
+    <!--    </div>-->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {},
+    computed: {
+      user() {
+        return this.$store.state.user;
+      }
+    }
   }
-}
 </script>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+
+  .centered > * {
+    text-align: center;
+  }
+</style>
